fd8d9e0cc30ce369bc5e53cef0a74486d7b04242
Robin Roussel <robin.roussel@inria.fr>
2017-04-11 14:48:25 +0100

    Add experimental support for face normals / flat shading.

--- utils.py
+++ utils.py
@@ -7,12 +7,19 @@ Utility functions
 """
 from panda3d.core import (Geom, GeomVertexFormat, GeomVertexWriter,
                           GeomVertexData, GeomTriangles)
+import solid
+import trimesh
 
-def trimesh2panda(vertices, triangles, normals=None, colors=None):
+
+def trimesh2panda(vertices, triangles, vertex_normals=None, face_normals=None,
+                  colors=None, flat_shading=False):
     """Takes triangular mesh data and returns a Panda3D Geom object."""
-    # Choose the correct vertex data format
-    has_colors = colors is not None
+    assert(flat_shading == (face_normals is not None))
+    # Flags
+    normals = face_normals if flat_shading else vertex_normals
     has_normals = normals is not None
+    has_colors = colors is not None
+    # Choose the correct vertex data format
     if has_normals:
         if has_colors:
             fmt = GeomVertexFormat.getV3n3c4()
@@ -25,6 +32,7 @@ def trimesh2panda(vertices, triangles, normals=None, colors=None):
 
     vdata = GeomVertexData("vertices", fmt, Geom.UHStatic)
     vdata.setNumRows(len(vertices))
+#    print(len(vertices))
 
     # Add vertex position
     writer = GeomVertexWriter(vdata, "vertex") # Name is not arbitrary here!
@@ -34,10 +42,16 @@ def trimesh2panda(vertices, triangles, normals=None, colors=None):
     # Add vertex normals
     if has_normals:
         writer = GeomVertexWriter(vdata, "normal") # Name is not arbitrary here!
-        for normal in normals:
-            writer.addData3f(*normal)
+        if flat_shading:
+            for normal in normals:
+                writer.addData3f(0, 0, 0)
+                writer.addData3f(0, 0, 0)
+                writer.addData3f(*normal)
+        else:
+            for normal in normals:
+                writer.addData3f(*normal)
 
-    # Add vertex color if there's one
+    # Add vertex color
     if has_colors:
         writer = GeomVertexWriter(vdata, "color") # Name is not arbitrary here!
         for color in colors:
@@ -45,6 +59,8 @@ def trimesh2panda(vertices, triangles, normals=None, colors=None):
 
     # Make primitives and assign vertices to them
     gtris = GeomTriangles(Geom.UHStatic)
+    if flat_shading:
+        gtris.setShadeModel(Geom.SM_flat_last_vertex)
     for triangle in triangles:
         gtris.addVertices(*triangle)
         gtris.closePrimitive()
@@ -54,3 +70,13 @@ def trimesh2panda(vertices, triangles, normals=None, colors=None):
     geom.addPrimitive(gtris)
 
     return geom
+
+
+# TODO: Rather than using being hackish with trimesh, implement a proper
+# method that generates a STL tempfile, converts it to an EGG tempfile, then
+# loads it.
+def solid2panda(model):
+    scad = solid.scad_render(model).replace('$', '$$')
+    data = trimesh.interfaces.scad.interface_scad([], scad)
+    return trimesh2panda(data['vertices'], data['faces'],
+                         face_normals=data['face_normals'], flat_shading=True)
