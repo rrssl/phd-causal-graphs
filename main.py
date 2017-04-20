8dbfe94eaaf220da78115a3c4c23c23604360d37
Robin Roussel <robin.roussel@inria.fr>
2017-04-20 11:01:46 +0100

    Add the first UI functionalities.

--- main.py
+++ main.py
@@ -5,60 +5,90 @@ Playing with Panda3D and Bullet
 
 @author: Robin Roussel
 """
-from direct.showbase.ShowBase import ShowBase
-from panda3d.core import Vec3, NodePath
-from panda3d.bullet import (BulletWorld, BulletPlaneShape, BulletRigidBodyNode,
-                            BulletSphereShape)
+import sys
 
-from procedural import make_plane
+#from direct.filter.CommonFilters import CommonFilters
+from direct.gui.DirectGui import DirectButton
+from panda3d.core import Vec3
+from panda3d.core import load_prc_file_data
+load_prc_file_data("", "win-size 1600 900")
+load_prc_file_data("", "window-title RGM Designer")
 
 
-class MyApp(ShowBase):
+#from primitives import BallRun, Floor, DominoRun
+from uimixins import Tileable, Focusable, Drawable
+from viewers import Modeler
+
+
+
+
+
+class MyApp(Tileable, Focusable, Drawable, Modeler):
+
     def __init__(self):
-        super().__init__(self)
+        Modeler.__init__(self)
+        Tileable.__init__(self)
+        Focusable.__init__(self)
+        Drawable.__init__(self)
 
-        # Camera
-        self.cam.setPos(20, 20, 40)
-        self.cam.lookAt(0, 0, 0)
+        # Initial camera position
+        self.min_cam_distance = .5
+        self.cam_distance = 30.
+        self.pivot.set_hpr(Vec3(135, 30, 0))
 
-        # World
-        world = BulletWorld()
-        world.setGravity(Vec3(0, 0, -9.81))
+        # Controls
+        self.accept("escape", sys.exit)
 
-        # Plane
-        shape = BulletPlaneShape(Vec3(0, -0.1, 0.9), 1)
-        node = BulletRigidBodyNode('Ground')
-        node.addShape(shape)
-        np = self.render.attachNewNode(node)
-        np.setPos(0, 0, -2)
-        world.attachRigidBody(node)
-        model = NodePath(make_plane())
-        model.reparentTo(np)
+        # TODO.
+        # Slide in (out) menu when getting in (out of) focus
+        # Smooth the drawing curve
+        # Sample it and instantiate dominos
 
-        # Box
-        shape = BulletSphereShape(0.5)
-        node = BulletRigidBodyNode('Ball')
-        node.setMass(1.0)
-        node.addShape(shape)
-        np = self.render.attachNewNode(node)
-        np.setPos(0, 0, 2)
-        world.attachRigidBody(node)
-        model = self.loader.loadModel('models/smiley.egg')
-        model.flattenLight()
-        model.reparentTo(np)
+#        self.gui = self.render.attach_new_node("gui")
+        font = self.loader.load_font("assets/Roboto_regular.ttf")
+        self.add_bt = DirectButton(
+                command=self.click_add_button,
+                # Background
+                relief='flat',
+                frameColor=(65/255, 105/255, 225/255, 1),
+                pad=(.75, .75),
+                # Text
+                text="+ ADD",
+                text_font=font,
+                text_fg=(1, 1, 1, 1),
+                # Position and scale
+                parent=self.a2dBottomRight,
+                pos=Vec3(-.2, 0, .2*9/16),
+                scale=.04)
 
-        self.world = world
-        self.taskMgr.add(self.update, 'update')
+    def click_add_button(self):
+        self.set_show_tile(True)
+        self.acceptOnce("mouse1", self.focus_on_tile)
 
-    # Update
-    def update(self, task, speedup=1):
-      dt = self.taskMgr.globalClock.getDt()
-      self.world.doPhysics(speedup*dt, 1, speedup/60)
-      return task.cont
+    def focus_on_tile(self):
+        self.focus_view(self.tile)
+        self.acceptOnce("c", self.cancel_add_primitive)
+        self.start_drawing()
+
+    def cancel_add_primitive(self):
+        self.stop_drawing()
+        self.set_show_tile(False)
+        self.unfocus_view()
+
+    def start_drawing(self):
+        self.accept("mouse1", self.set_draw, [True])
+        self.accept("mouse1-up", self.set_draw, [False])
+
+    def stop_drawing(self):
+        self.ignore("mouse1")
+        self.ignore("mouse1-up")
+        self.clear_drawing()
+
 
 def main():
     app = MyApp()
     app.run()
+
 
 if __name__ == "__main__":
     main()
---
+++ uimixins.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+"""
+Various UI functionalities.
+
+@author: Robin Roussel
+"""
+import math
+import numpy as np
+
+from direct.interval.IntervalGlobal import Func, LerpFunc, Parallel, Sequence
+from panda3d.core import CardMaker
+from panda3d.core import LineSegs
+from panda3d.core import Plane, Point3, Vec3
+
+
+class Focusable:
+    """Mixin to add smooth focus functionality to a Modeler.
+
+    """
+    def __init__(self):
+        super().__init__()
+
+        self._focus_anim = None
+        self._focused = False
+
+    def focus_view(self, nodepath):
+        if (self._focused
+            or (self._focus_anim is not None
+                and self._focus_anim.is_playing())):
+            return
+        # Get new desired state
+        bounds = nodepath.get_bounds()
+        center = bounds.get_center()
+        radius = bounds.get_radius()
+        lens = self.camLens
+        fov = min(lens.get_fov()) * math.pi / 180  # min between X and Z axes
+        distance = radius / math.tan(fov * .5)
+#        idealFarPlane = distance + radius * 1.5
+#        lens.setFar(max(lens.getDefaultFar(), idealFarPlane))
+#        idealNearPlane = distance - radius
+#        lens.setNear(min(lens.getDefaultNear(), idealNearPlane))
+#
+        # Save original state
+        self._unfocus_state = {
+                'pos': self.pivot.get_pos(),
+                'hpr': self.pivot.get_hpr(),
+                'zoom': self.cam_distance
+                }
+        # Launch animation
+        time = 1.
+        # Note: using the Quat version ensures that the rotation takes the
+        # shortest path. We cam still give it an HPR argument, which is
+        # (I think) easier to visualize than
+        # "Quat(0, 0, 1/sqrt(2), 1/sqrt(2))".
+        change_rigid = self.pivot.posQuatInterval(
+                duration=time,
+                pos=center + Vec3(0, 0, distance),
+                hpr=Vec3(180, 90, 0),
+                blendType='easeOut')
+        change_zoom = LerpFunc(
+                lambda x: setattr(self, "cam_distance", x),
+                duration=time,
+                fromData=self.cam_distance,
+                toData=self.min_cam_distance,
+                blendType='easeOut')
+        self._focus_anim = Sequence(
+                Func(lambda: setattr(self, "move_highlight", False)),
+                Parallel(change_rigid, change_zoom),
+                Func(lambda: setattr(self, "_focused", True)))
+        self._focus_anim.start()
+
+    def unfocus_view(self):
+        if (not self._focused
+            or (self._focus_anim is not None
+                and self._focus_anim.is_playing())):
+            return
+        # Launch animation
+        time = 1.
+        change_rigid = self.pivot.posQuatInterval(
+                duration=time,
+                pos=self._unfocus_state['pos'],
+                hpr=self._unfocus_state['hpr'],
+                blendType='easeOut')
+        change_zoom = LerpFunc(
+                lambda x: setattr(self, "cam_distance", x),
+                duration=time,
+                fromData=self.cam_distance,
+                toData=self._unfocus_state['zoom'],
+                blendType='easeOut')
+        self._focus_anim = Sequence(
+                Parallel(change_rigid, change_zoom),
+                Func(lambda: setattr(self, "move_highlight", True)),
+                Func(lambda: setattr(self, "_focused", False)))
+        self._focus_anim.start()
+
+
+class Tileable:
+    """Mixin adding a visual tile selector to a Modeler.
+
+    """
+    def __init__(self):
+        self.plane = Plane(Vec3(0, 0, 1), Point3(0, 0, 0))
+        self.tlims = 9
+
+        cm = CardMaker("tile")
+        cm.set_frame(-1, 1, -1, 1)
+        cm.set_color(0, 1, 0, .4)
+        self.tile = self.visual.attach_new_node(cm.generate())
+        self.tile.look_at(0, 0, -1)
+        self.tile.set_two_sided(True)
+        self.tile.set_transparency(True)
+#        filters = CommonFilters(self.win, self.cam)
+#        filters.setBloom()
+#        self.tile.setShaderAuto()
+        self.tile.hide()
+
+        self.move_highlight = False
+
+    def set_show_tile(self, show):
+        if show:
+            self.tile.show()
+            self.task_mgr.add(self.highlight_tile, "highlight_tile")
+        else:
+            self.tile.hide()
+            self.task_mgr.remove("highlight_tile")
+        self.move_highlight = show
+
+    def highlight_tile(self, task):
+        if self.move_highlight and self.mouseWatcherNode.has_mouse():
+            mpos = self.mouseWatcherNode.get_mouse()
+            pos3d = Point3()
+            near_point = Point3()
+            far_point = Point3()
+            self.camLens.extrude(mpos, near_point, far_point)
+            if self.plane.intersects_line(
+                    pos3d,
+                    self.render.get_relative_point(self.camera, near_point),
+                    self.render.get_relative_point(self.camera, far_point)
+                    ):
+                pos3d = np.asarray(pos3d).clip(-self.tlims, self.tlims).round()
+                self.tile.set_pos(self.render, *pos3d)
+        return task.cont
+
+
+class Drawable:
+    """Mixin giving to ShowBase the ability to sketch on the screen.
+
+    """
+    def __init__(self):
+        # Not the most efficient structure but avoids a lot of low-level code.
+        self.strokes = LineSegs("polyline")
+        self.strokes.set_thickness(2)
+        self.strokes.set_color((0, 0, 1, 1))
+        self.sketch_np = None
+
+    def set_draw(self, draw):
+        if draw:
+            if self.mouseWatcherNode.has_mouse():
+                pos = self.mouseWatcherNode.get_mouse()
+                self.strokes.move_to(pos[0], 0, pos[1])
+            self.task_mgr.add(self.update_drawing, "update_drawing")
+        else:
+            self.task_mgr.remove("update_drawing")
+
+    def update_drawing(self, task):
+        if self.mouseWatcherNode.has_mouse():
+            pos = self.mouseWatcherNode.get_mouse()
+            self.strokes.draw_to(pos[0], 0, pos[1])
+
+            # Update the drawing
+            # Use a copy because create() calls reset().
+            geom = LineSegs(self.strokes).create()
+            try:
+                # I don't know if this is more efficient than calling
+                # self.sketch_np.remove_node()
+                # and then attach a new node every time.
+                geom.replace_node(self.sketch_np.node())
+            except AttributeError:
+                self.sketch_np = self.render2d.attach_new_node(geom)
+            except AssertionError:
+                if self.sketch_np.is_empty():
+                    self.sketch_np = self.render2d.attach_new_node(geom)
+
+        return task.cont
+
+    def clear_drawing(self):
+        try:
+            self.sketch_np.remove_node()
+        except AttributeError:
+            pass
+        self.strokes.reset()
