be1242ef97692fb9f13744ba88f31a0d2add45bf
Robin Roussel <robin.roussel@inria.fr>
2017-07-01 11:03:34 +0100

    Add plotting of variance computations.

--- variance.py
+++ variance.py
@@ -6,6 +6,7 @@ Determining variances in the sytem.
 @author: Robin Roussel
 """
 import math
+import matplotlib.pyplot as plt
 import numpy as np
 from panda3d.bullet import BulletWorld
 from panda3d.core import NodePath
@@ -97,8 +98,33 @@ def run_xp1():
             probas[i, j] = success.count(True) / nb_trials
     print("Probabilities of success:\n", probas)
 
+    # Process results
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_aspect(1)
+    res = ax.imshow(probas, cmap=plt.cm.viridis,
+                    interpolation='nearest')
 
-nb_trials = 20
+    width, height = probas.shape
+
+    for x in range(width):
+        for y in range(height):
+            ax.annotate(str(probas[x][y]), xy=(y, x),
+                        horizontalalignment='center',
+                        verticalalignment='center')
+
+    cb = fig.colorbar(res, ticks=[0., 1.])
+    plt.xticks(range(height), step)
+    plt.yticks(range(width), nb_dominos[::-1])
+    ax.set_xticks([.5, 1.5, 2.5, 3.5, 4.5], minor=True);
+    ax.set_yticks([.5, 1.5, 2.5, 3.5, 4.5], minor=True);
+    ax.tick_params(which='both', length=0)
+    ax.grid(which='minor', color='k', linestyle='-', linewidth=1)
+    ax.set_xlabel("Space between dominoes")
+    ax.set_ylabel("Number of dominoes")
+    plt.show()
+
+
 def run_xp2():
     print("XP2: measuring probability of success wrt a single source of "
           "error.")
@@ -125,6 +151,14 @@ def run_xp2():
             success.append(all(toppled(d) for d in dom_path.get_children()))
         probas[i] = success.count(True) / nb_trials
     print("Probabilities of success:\n", probas)
+
+    # Process results
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(alpha, probas)
+    ax.set_xlabel("Symmetric uniform distribution parameter (in degrees)")
+    ax.set_ylabel("Probability of success")
+    plt.show()
 
 
 def run_xp3():
@@ -158,7 +192,18 @@ def run_xp3():
             probas[i, k] = success.count(True) / nb_trials
     print("Probabilities of success:\n", probas)
 
+    # Process results
+    fig, axes = plt.subplots(2, 3, sharex=True, sharey=True)
+    for i in range(len(step)):
+        # ax = fig.add_subplot(2, 3, i+1)
+        axes.flat[i].scatter(alpha, probas[i])
+        axes.flat[i].set_title("Step = {:.2f}".format(step[i]))
+        # ax.set_xlabel("Symmetric uniform distribution parameter (in degrees)")
+        # ax.set_ylabel("Probability of success")
+    plt.show()
 
-# run_xp1()
+
+run_xp1()
+# nb_trials = 30
 # run_xp2()
-run_xp3()
+# run_xp3()
