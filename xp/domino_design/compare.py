4e90e1f0b09d7b25ad5aebcc8954b40e8c7d7899
Robin Roussel <robin.roussel@inria.fr>
2017-08-28 11:54:51 +0100

    Define all input paths inside compare.py rather than via command line arguments.

--- xp/domino_design/compare.py
+++ xp/domino_design/compare.py
@@ -1,16 +1,7 @@
 """
 Build the various statistics used to compare the domino distribution methods.
 
-Parameters
-----------
-datapath : string
-  Path where the data lies.
-
 """
-import glob
-import os
-import sys
-
 import numpy as np
 from tabulate import tabulate
 
@@ -23,37 +14,53 @@ METHODS = (
         )
 HEADERS = (
         "Method",
-        "Success rate",
+        "Fully covered path",
+        "No overlap",
+        "All dominoes topple",
+        "Overall success",
+        "False start",
         "Time/domino (ms)",
-        "False start rate"
+        )
+DOM_FILES = (
+        "test-100/dominoes-method_1.npz",
+        "test-100/test-dominoes-method_2.npz",
+        "test-100/test-dominoes-method_3.npz",
+        "test-100/dominoes-method_4.npz",
+        )
+VAL_FILES = (
+        "test-100/validity-method_1.npy",
+        "test-100/test-dominoes-method_2-validity.npy",
+        "test-100/test-dominoes-method_3-validity.npy",
+        "test-100/validity-method_4.npy",
+        )
+TIME_FILES = (
+        "test-100/times-method_1.npy",
+        "test-100/test-times-method_2.npy",
+        "test-100/test-times-method_3.npy",
+        "test-100/times-method_4.npy",
         )
 
 
 def main():
-    if len(sys.argv) > 2:
-        print(__doc__)
-        return
-    datapath = sys.argv[1]
-    dominoes_files = glob.glob(os.path.join(datapath, "dominoes*.npz"))
-    validity_files = glob.glob(os.path.join(datapath, "validity*.npy"))
-    times_files = glob.glob(os.path.join(datapath, "times*.npy"))
-    dominoes_files.sort()
-    validity_files.sort()
-    times_files.sort()
-
     table = []
     for method, domfile, valfile, timefile in zip(
-            METHODS, dominoes_files,validity_files, times_files):
+            METHODS, DOM_FILES, VAL_FILES, TIME_FILES):
         domarrays = np.load(domfile)
         ndomarray = np.array([len(domarrays['arr_{}'.format(i)])
                      for i in range(len(domarrays.files))])
         valarray = np.load(valfile)
+        overallarray = np.logical_and(
+                valarray[:, 0],
+                np.logical_and(valarray[:, 1], valarray[:, 2]))
         timearray = np.load(timefile)
         table.append([
             method,
-            sum(valarray) / len(valarray),
-            (timearray / ndomarray).mean() * 1000,
+            sum(valarray[:, 0]) / len(valarray),
+            sum(valarray[:, 1]) / len(valarray),
+            sum(valarray[:, 2]) / len(valarray),
+            sum(overallarray) / len(overallarray),
             sum(ndomarray == 1) / len(ndomarray),
+            (timearray / ndomarray).mean() * 1000,
             ])
 
     print(tabulate(table, headers=HEADERS))
