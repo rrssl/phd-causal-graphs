6acc85ffb0011f004fcc5d5a4b336d31e8892842
Robin Roussel <robin.roussel@inria.fr>
2017-09-12 11:20:02 +0100

    Make figures in compare.py

--- xp/domino_design/compare.py
+++ xp/domino_design/compare.py
@@ -4,8 +4,10 @@ Build the various statistics used to compare the domino distribution methods.
 """
 import numpy as np
 from tabulate import tabulate
+import matplotlib.pyplot as plt
 
 
+MAKE_FIGURES = True
 METHODS = (
         "Equal spacing",
         "Minimal spacing",
@@ -21,15 +23,14 @@ HEADERS = (
         "All dominoes topple",
         "Overall success",
         #  "False start",
-        "Success fraction",
         "Time/domino (ms)",
         )
 HEADERS2 = (
         "Method",
-        "Success frac.",
-        "Success frac. (5% uncert.)",
-        "Success frac. (10% uncert.)",
-        "Success frac. (15% uncert.)",
+        "Success fraction",
+        "-- with 5% error",
+        "-- with 10% error",
+        "-- with 15% error",
         )
 FILES_PREFIX = "data/20170909-2/"
 DOM_FILES = [
@@ -67,11 +68,6 @@ def main():
             #  sum(ndomarray == 1) / len(ndomarray),
             (timearray / ndomarray).mean() * 1000,
             ])
-
-    print(tabulate(table, headers=HEADERS))
-
-    print('\n')
-
     table2 = []
     for method, valfile in zip(METHODS, VAL_FILES):
         domarrays = np.load(domfile)
@@ -83,9 +79,42 @@ def main():
             valarray[:, 5].mean(),
             valarray[:, 6].mean(),
             ])
-
+    print(tabulate(table, headers=HEADERS))
+    print('\n')
     print(tabulate(table2, headers=HEADERS2))
 
+    if not MAKE_FIGURES:
+        return
+    # First figure
+    fig, ax = plt.subplots(figsize=(9, 6))
+    index = np.arange(4)
+    bar_width = .1
+    maxtime = max(time for *_, time in table)
+    for i, (method, *data) in enumerate(table):
+        ax.bar(index + i * bar_width, data[:3] + [data[4] / maxtime],
+               bar_width, label=method)
+    ax.set_xticks(index + bar_width*(len(table)-1)/2)
+    ax.set_xticklabels(HEADERS[1:4] + (HEADERS[5] + "\n(rel. to max)",))
+    ax.set_ylabel("Percentage of curves following the criterion")
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, labels, loc='upper center', bbox_to_anchor=(.5, -.05),
+              borderaxespad=2, ncol=2)
+    ax.set_title("Comparison of binary validity criteria and time")
+    plt.savefig("validity.png", bbox_inches='tight')
+    # Second figure
+    fig, ax = plt.subplots()
+    x = (0, .05, .1, .15)
+    for method, *data in table2:
+        ax.plot(x, data, label=method, marker='o')
+    ax.set_xticks(x)
+    ax.set_ylim(0, 1)
+    ax.set_xlabel("Percentage of error in domino placement")
+    ax.set_ylabel("Fraction of the path that toppled")
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, labels)
+    ax.set_title("Comparison of success fraction as a function of error")
+    fig.tight_layout()
+    plt.savefig("uncertainty.png")
 
 if __name__ == "__main__":
     main()
