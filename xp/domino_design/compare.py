f816810324e60119073d33347d8990912c231ee2
Robin Roussel <robin.roussel@inria.fr>
2017-09-12 18:12:30 +0100

    Add metrics comparison in compare.py

--- xp/domino_design/compare.py
+++ xp/domino_design/compare.py
@@ -32,6 +32,10 @@ HEADERS2 = (
         "-- with 10% error",
         "-- with 15% error",
         )
+HEADERS3 = (
+        "Method",
+        "Density",
+        )
 FILES_PREFIX = "data/20170909-2/"
 DOM_FILES = [
         FILES_PREFIX + "candidates-dominoes-method_{}.npz".format(i)
@@ -45,33 +49,36 @@ TIME_FILES = [
         FILES_PREFIX + "candidates-times-method_{}.npy".format(i)
         for i in range(1, len(METHODS)+1)
         ]
+MET_FILES = [
+        FILES_PREFIX + "candidates-dominoes-method_{}-metrics.npy".format(i)
+        for i in range(1, len(METHODS)+1)
+        ]
 
 
 def main():
     table = []
-    for method, domfile, valfile, timefile in zip(
-            METHODS, DOM_FILES, VAL_FILES, TIME_FILES):
+    table2 = []
+    table3 = []
+    for method, domfile, valfile, timefile, metfile in zip(
+            METHODS, DOM_FILES, VAL_FILES, TIME_FILES, MET_FILES):
         domarrays = np.load(domfile)
         ndomarray = np.array([len(domarrays['arr_{}'.format(i)])
-                     for i in range(len(domarrays.files))])
+                              for i in range(len(domarrays.files))])
         valarray = np.load(valfile)
         overallarray = np.logical_and(
                 valarray[:, 0],
                 np.logical_and(valarray[:, 1], valarray[:, 2]))
         timearray = np.load(timefile)
+        metarray = np.load(metfile)
         table.append([
             method,
             valarray[:, 0].mean(),
             valarray[:, 1].mean(),
             valarray[:, 2].mean(),
             overallarray.mean(),
-            #  sum(ndomarray == 1) / len(ndomarray),
+            #  (ndomarray == 1) .mean(),
             (timearray / ndomarray).mean() * 1000,
             ])
-    table2 = []
-    for method, valfile in zip(METHODS, VAL_FILES):
-        domarrays = np.load(domfile)
-        valarray = np.load(valfile)
         table2.append([
             method,
             valarray[:, 3].mean(),
@@ -79,9 +86,15 @@ def main():
             valarray[:, 5].mean(),
             valarray[:, 6].mean(),
             ])
+        table3.append([
+            method,
+            metarray[:, 0].mean(),
+            ])
     print(tabulate(table, headers=HEADERS))
     print('\n')
     print(tabulate(table2, headers=HEADERS2))
+    print('\n')
+    print(tabulate(table3, headers=HEADERS3))
 
     if not MAKE_FIGURES:
         return
@@ -115,6 +128,23 @@ def main():
     ax.set_title("Comparison of success fraction as a function of error")
     fig.tight_layout()
     plt.savefig("uncertainty.png")
+    # Third figure
+    fig, ax = plt.subplots()
+    index = np.arange(1)
+    bar_width = .01
+    for i, ((method, *means), err) in enumerate(zip(table3, table3_err)):
+        ax.bar(index + i*bar_width, means[:3], bar_width, label=method)
+        ax.errorbar(index + i*bar_width, means[:3], yerr=err[:3],
+                    fmt='none', capsize=5, ecolor='k')
+    #  ax.set_xlim(-.03, .05)
+    ax.set_ylim(0, 1)
+    ax.set_xticks(index + bar_width*(len(table)-1)/2)
+    ax.set_xticklabels(HEADERS3[1:])
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, labels)
+    ax.set_title("Metrics comparisons for different spacing values")
+    plt.savefig(FILES_PREFIX + "metrics.png", bbox_inches='tight')
+
 
 if __name__ == "__main__":
     main()
