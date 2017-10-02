Fix error in local coordinates computation + Refactor

xp/predicting_domino_toppling/evaluate.py
@@ -29,32 +29,14 @@ import spline2d as spl
 
 sys.path.insert(0, os.path.abspath(".."))
 from predicting_domino_toppling.config import X_MAX, Y_MAX, A_MAX
+from predicting_domino_toppling.functions import get_rel_coords
 
 
 def eval_pairs_in_distrib(u, spline, classifier):
-    # Get local Cartesian coordinates
-    # Change origin
-    xi, yi = spl.splev(u, spline)
-    xi = xi[1:] - xi[:-1]
-    yi = yi[1:] - yi[:-1]
-    # Rotate by -a_i-1
-    ai = spl.splang(u, spline, degrees=False)
-    ci_ = np.cos(ai[:-1])
-    si_ = np.sin(ai[:-1])
-    xi = xi*ci_ + yi*si_
-    yi = -xi*si_ + yi*ci_
-    # Get relative angles
-    ai = np.degrees(ai[1:] - ai[:-1])
-    ai = (ai + 180) % 360 - 180  # Convert from [0, 360) to [-180, 180)
-    # Symmetrize
-    ai = np.copysign(ai, yi)
-    yi = abs(yi)
     # Normalize
-    xi /= X_MAX
-    yi /= Y_MAX
-    ai /= A_MAX
+    xya = get_rel_coords(u, spline) / (X_MAX, Y_MAX, A_MAX)
     # Evaluate
-    return classifier.predict(np.column_stack((xi, yi, ai)))
+    return classifier.predict(xya)
 
 
 def get_estimated_toppling_fraction(u, spline, pairwise_topple):
@@ -121,8 +103,6 @@ def main():
 
     print("\nClassifier performance over entire chains")
     r2_score = metrics.r2_score(simulated_failpoint, predicted_failpoint)
-    expvar_score = metrics.explained_variance_score(
-            simulated_failpoint, predicted_failpoint)
     mean_abserror = metrics.mean_absolute_error(
             simulated_failpoint, predicted_failpoint)
     med_abserror = metrics.median_absolute_error(
@@ -130,7 +110,6 @@ def main():
     print("R^2 score: ", r2_score)
     print("Mean absolute error: ", mean_abserror)
     print("Median absolute error: ", med_abserror)
-
 
     # Estimate correlation between chain length and estimation error
     print("\nCorrelation between chain length and estimation error")

xp/predicting_domino_toppling/functions.py
@@ -1,9 +1,12 @@
 """
-Test if two boxes collide.
+Functions used to train and evaluate the classifier.
+
 """
 from math import atan, pi
 import os
 import sys
+
+import numpy as np
 
 from panda3d.bullet import BulletWorld, BulletBoxShape, BulletRigidBodyNode
 from panda3d.core import load_prc_file_data
@@ -12,6 +15,7 @@ from panda3d.core import Point3, Vec3
 
 sys.path.insert(0, os.path.abspath("../.."))
 from primitives import Floor, DominoMaker
+import spline2d as spl
 
 
 # The next line avoids a "memory leak" that notably happens when
@@ -129,6 +133,29 @@ def run_domino_toppling_xp(params, timestep, maxtime, visual=False):
             world.do_physics(timestep, 2, timestep)
 
         return d2.get_r() >= toppling_angle
+
+
+def get_rel_coords(u, spline):
+    """Get the relative configuration of each domino wrt the previous."""
+    # Get local Cartesian coordinates
+    # Change origin
+    xi, yi = spl.splev(u, spline)
+    xi = np.diff(xi)
+    yi = np.diff(yi)
+    # Rotate by -a_i-1
+    ai = spl.splang(u, spline, degrees=False)
+    ci_ = np.cos(ai[:-1])
+    si_ = np.sin(ai[:-1])
+    xi_r = xi*ci_ + yi*si_
+    yi_r = -xi*si_ + yi*ci_
+    # Get relative angles
+    ai = np.degrees(np.diff(ai))
+    ai = (ai + 180) % 360 - 180  # Convert from [0, 360) to [-180, 180)
+    # Symmetrize
+    ai = np.copysign(ai, yi_r)
+    yi_r = np.abs(yi_r)
+
+    return np.column_stack((xi_r, yi_r, ai))
 
 
 def test_contact():

xp/predicting_domino_toppling/learning.py
@@ -37,7 +37,7 @@ def main():
     samples /= den
     values = np.load(vpath)
     svc = svm.SVC(
-            kernel='rbf', gamma=.1, C=.5, cache_size=1024,
+            kernel='rbf', gamma=1, C=1, cache_size=1024,
             #  class_weight='balanced',
             ).fit(samples, values)
 