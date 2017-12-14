98cf8c2f1229ed22326261287e47f0a5b853d34e
Robin Roussel <robin.roussel@inria.fr>
2017-12-14 16:01:19 +0000

    Change the training set generation script to accept the scenario number as a parameter

--- xp/learning_robustness/gen_training_set.py
+++ xp/learning_robustness/gen_training_set.py
@@ -3,7 +3,7 @@ Sample the parameter space for robustness learning.
 
 Parameters
 ----------
-scenario : int
+sid : int
   Sampling scenario. See sampling_methods.py for a description.
 nsam : int
   Number of samples.
@@ -22,13 +22,13 @@ def main():
     if len(sys.argv) < 3:
         print(__doc__)
         return
-    scenario = int(sys.argv[1])
+    sid = int(sys.argv[1])
     n = int(sys.argv[2])
 
+    generator_rule = 'H'
     filter_rules = dict(filter_overlap=True, tilt_first_domino=True)
-    samples = sample(n, scenario=Scenario(scenario), generator_rule='R',
-                     filter_rules=filter_rules)
-    name = "S{}-{}samples.npy".format(scenario, n)
+    samples = sample(n, Scenario(sid), generator_rule, filter_rules)
+    name = "S{}{}-{}samples.npy".format(sid, generator_rule, n)
     np.save(name, samples)
 
 
