d1fd6a2483f7757b9a97b359d8f99888aac56013
Robin Roussel <robin.roussel@inria.fr>
2017-12-20 15:36:53 +0100

    Use a standard scaler instead of the sampling bounds

--- xp/learning_robustness/train_estimator.py
+++ xp/learning_robustness/train_estimator.py
@@ -15,9 +15,11 @@ import sys
 import numpy as np
 from sklearn.externals import joblib
 from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 
-from config import X_MAX, Y_MAX, A_MAX, NCORES
+from config import NCORES
 
 
 def main():
@@ -27,36 +29,27 @@ def main():
     spath = sys.argv[1]
     vpath = sys.argv[2]
     samples = np.load(spath)
-    # Normalize
-    if samples.shape[1] == 2:
-        den = (X_MAX, A_MAX)
-    elif samples.shape[1] == 3:
-        den = (X_MAX, Y_MAX, A_MAX)
-    else:
-        print("Cannot normalize for this number of dimensions.")
-        return
-    samples /= den
     values = np.load(vpath)
+
+    pipeline = make_pipeline(
+            StandardScaler(),
+            SVC(kernel='rbf', random_state=123, cache_size=512),
+            )
 
     C_range = np.logspace(-3, 3, 7)
     gamma_range = np.logspace(-3, 3, 7)
     class_weight_options = [None, 'balanced']
     param_grid = {
-            'gamma': gamma_range,
-            'C': C_range,
-            'class_weight': class_weight_options
+            'svc__gamma': gamma_range,
+            'svc__C': C_range,
+            'svc__class_weight': class_weight_options
             }
-    grid = GridSearchCV(SVC(kernel='rbf', random_state=123),
-                        param_grid=param_grid,
-                        n_jobs=NCORES)
+
+    grid = GridSearchCV(pipeline, param_grid=param_grid, n_jobs=NCORES)
     grid.fit(samples, values)
     print("The best parameters are {} with a score of {}".format(
         grid.best_params_, grid.best_score_))
-    #  svc = svm.SVC(
-    #          kernel='rbf', gamma=1, C=1, cache_size=1024,
-    #          #  class_weight='balanced',
-    #          ).fit(samples, values)
-    #  print("Score: ", svc.score(samples, values))
+
     root, _ = os.path.splitext(spath)
     joblib.dump(grid.best_estimator_, root + "-classifier.pkl")
 
