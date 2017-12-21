df87c9a9dd130f4301facf947dda98b28ffc17c3
Robin Roussel <robin.roussel@inria.fr>
2017-12-21 18:06:59 +0100

    Rewrite eval_training (use new interfaces + simulate on the fly)

--- xp/learning_robustness/eval_training.py
+++ xp/learning_robustness/eval_training.py
@@ -13,7 +13,6 @@ spath : string
   Path to the .pkl file of splines.
 
 """
-import glob
 import os
 import pickle
 import sys
@@ -21,32 +20,30 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import linregress
-from sklearn.externals import joblib
 from sklearn import metrics
+from sklearn.externals import joblib
 
-from config import X_MAX, Y_MAX, A_MAX
+from config import NCORES
 sys.path.insert(0, os.path.abspath("../.."))
-import spline2d as spl
-from xp.dompath import get_rel_coords
+import spline2d as spl  # noqa
+from xp.domino_design.methods import equal_spacing  # noqa
+from xp.domino_predictors import DominoRobustness, DominoRobustness2  # noqa
+import xp.simulate as simu  # noqa
 
 
-def eval_pairs_in_distrib(u, spline, classifier):
-    # Normalize
-    xya = get_rel_coords(u, spline) / (X_MAX, Y_MAX, A_MAX)
-    # Evaluate
-    return classifier.predict(xya)
+def true_frac(a):
+    first_false = np.argmin(a)
+    if a[first_false]:
+        return 1
+    else:
+        return first_false / len(a)
 
 
-def get_estimated_toppling_fraction(u, spline, pairwise_topple):
-    n = len(pairwise_topple)
-    try:
-        # Find the first domino that didn't topple.
-        idx = next(i for i in range(n) if not pairwise_topple[i])
-    except StopIteration:
-        # All dominoes toppled
-        idx = n
-
-    return np.asscalar(spl.arclength(spline, u[idx]) / spl.arclength(spline))
+def compute_simulated_toppling_fraction(u, spline):
+    doms_np, world = simu.setup_dominoes_from_path(u, spline)
+    times = simu.run_simu(doms_np, world)
+    frac = true_frac(np.isfinite(times))
+    return frac
 
 
 def main():
@@ -55,61 +52,60 @@ def main():
         return
     cpath = sys.argv[1]
     spath = sys.argv[2]
-    root, _ = os.path.splitext(spath)
-    dpaths = glob.glob(root + "-dominoes-method_*.npz")
-    dpaths.sort()
-    vpaths = glob.glob(root + "-dominoes-method_*-validity.npy")
-    vpaths.sort()
 
-    classifier = joblib.load(cpath)
+    type_ = int(os.path.basename(cpath)[1])
+    if type_ == 2:
+        estimator = DominoRobustness(cpath)
+    elif type_ == 4:
+        estimator = DominoRobustness2(cpath)
     with open(spath, 'rb') as f:
         splines = pickle.load(f)
-    methods_dominoes = [np.load(dpath) for dpath in dpaths]
-    methods_validities = [np.load(vpath) for vpath in vpaths]
 
-    lengths = []
-    simulated_failpoint = []
-    predicted_failpoint = []
-    all_topple = []
-    all_topple_estimated = []
-    for dominoes, validities in zip(methods_dominoes, methods_validities):
-        for i, spline in enumerate(splines):
-            no_overlap = validities[i, 1]
-            if no_overlap:
-                u = dominoes['arr_{}'.format(i)]
-                lengths.append(len(u))
-                # Get toppling results obtained from simulation
-                all_topple.append(validities[i, 2])
-                simulated_failpoint.append(validities[i, 3])
-                # Estimate toppling with classifier
-                pairwise_topple = eval_pairs_in_distrib(u, spline, classifier)
-                predicted_failpoint.append(get_estimated_toppling_fraction(
-                        u, spline, pairwise_topple))
-                all_topple_estimated.append(pairwise_topple.all())
-    abs_errors = np.abs(np.subtract(simulated_failpoint, predicted_failpoint))
+    # Generate ground truth data.
+    root = os.path.splitext(spath)[0]
+    filename = root + "-simu-toppling.npz"
+    try:
+        data = np.load(filename)
+        simu_toppling = data['simu_toppling']
+        distribs = [data['arr_{}'.format(i)]
+                    for i in range(len(simu_toppling))]
+    except FileNotFoundError:
+        distribs = [equal_spacing(spline) for spline in splines]
+        simu_toppling = joblib.Parallel(n_jobs=NCORES)(
+                joblib.delayed(compute_simulated_toppling_fraction)(u, spline)
+                for u, spline in zip(distribs, splines))
+        np.savez(filename, *distribs, simu_toppling=simu_toppling)
+
+    # Generate predictions.
+    distribs_coords = [
+            np.column_stack(spl.splev(u, spline) + [spl.splang(u, spline)])
+            for u, spline in zip(distribs, splines)]
+    pred_toppling = np.array([
+            true_frac(estimator(coords) >= 0) for coords in distribs_coords])
 
     # Confusion matrix
-    conf_mat = metrics.confusion_matrix(all_topple, all_topple_estimated)
-    print("Classifier performance")
+    conf_mat = metrics.confusion_matrix(
+            simu_toppling == 1, pred_toppling == 1)
+    print("Classification performance over entire chains")
     print("Confusion matrix:\n", conf_mat)
     (tn, fp), (fn, tp) = conf_mat
     precision = tp / (tp + fp)
     recall = tp / (tp + fn)
-    accuracy = (tp + tn) / len(all_topple)
+    accuracy = (tp + tn) / len(splines)
     print("Precision = {}, recall = {}, accuracy = {}".format(
         precision, recall, accuracy))
 
-    print("\nClassifier performance over entire chains")
-    r2_score = metrics.r2_score(simulated_failpoint, predicted_failpoint)
-    mean_abserror = metrics.mean_absolute_error(
-            simulated_failpoint, predicted_failpoint)
-    med_abserror = metrics.median_absolute_error(
-            simulated_failpoint, predicted_failpoint)
+    print("\nSuccess fraction estimation performance over entire chains")
+    r2_score = metrics.r2_score(simu_toppling, pred_toppling)
+    mean_abserror = metrics.mean_absolute_error(simu_toppling, pred_toppling)
+    med_abserror = metrics.median_absolute_error(simu_toppling, pred_toppling)
     print("R^2 score: ", r2_score)
     print("Mean absolute error: ", mean_abserror)
     print("Median absolute error: ", med_abserror)
 
     # Estimate correlation between chain length and estimation error
+    abs_errors = np.abs(np.subtract(simu_toppling, pred_toppling))
+    lengths = [len(u) for u in distribs]
     print("\nCorrelation between chain length and estimation error")
     slope, intercept, r_value, p_value, std_err = linregress(
             lengths, abs_errors)
@@ -124,7 +120,7 @@ def main():
     bins = list(range(0, max(lengths), binsize))
     inds = np.digitize(lengths, bins) - 1
     abs_errors = np.array(abs_errors)
-    mean_abs_errors = [abs_errors[inds == i].mean() if (inds == i).any() else 0
+    mean_abs_errors = [abs_errors[inds == i].mean() if i in inds else 0
                        for i in range(len(bins))]
 
     fig, ax = plt.subplots()
