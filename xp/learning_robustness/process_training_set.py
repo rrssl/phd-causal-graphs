e185c955115d31ea763bb3a06ce7eff3f36c7766
Robin Roussel <robin.roussel@inria.fr>
2018-02-16 18:02:31 +0100

    Allow to pass arbitrary optional key-value pairs

--- xp/learning_robustness/process_training_set.py
+++ xp/learning_robustness/process_training_set.py
@@ -7,8 +7,10 @@ spath : string
   Path to the samples to process.
 sid : int
   ID of the scenario used to generate the samples. See gen_training_set.py.
-n : int, optional
-  Additional scenario parameter, depends on the scenario.
+argname : string, optional
+  Name of the additional scenario parameter.
+argval : int, optional
+  Value associated to the additional scenario parameter.
 
 """
 import os
@@ -22,8 +24,10 @@ sys.path.insert(0, os.path.abspath("../.."))
 from xp.simulate import Simulation  # noqa: E402
 from xp.scenarios import SCENARIOS  # noqa: E402
 
+VISUAL = 0
 
-def process(scenario, sample, n=0, visual=False):
+
+def process(scenario, sample, visual=False, **kwargs):
     """
     Run the simulation. If not visual, returns True if the second domino
     topples.
@@ -34,21 +38,21 @@ def process(scenario, sample, n=0, visual=False):
       Class of the chosen scenario (from scenarios.py).
     sample : array
       Parameters for this instance of the scenario.
-    n : int, optional
-      Additional parameter for the scenario.
     visual : bool, optional
       Run the experiment in 'visual' mode, that is, actually display the
       scene in a window.
+    kwargs : dict, optional
+      Key-value pair of additional parameters for the scenario.
 
     """
-    instance = scenario(sample, n, make_geom=visual)
+    instance = scenario(sample, make_geom=visual, **kwargs)
     simu = Simulation(instance)
 
     if visual:
         simu.run_visual()
-        return True
+    else:
+        simu.run()
 
-    simu.run()
     return instance.succeeded()
 
 
@@ -58,15 +62,15 @@ def main():
         return
     spath = sys.argv[1]
     sid = int(sys.argv[2])
-    try:
-        n = int(sys.argv[3])
-    except IndexError:
-        n = 0
+    argit = iter(sys.argv[3:])
+    kwargs = {argname: int(argval) for argname, argval in zip(argit, argit)}
+    print("kwargs: ", kwargs)
     scenario = SCENARIOS[sid]
     samples = np.load(spath)
-    values = Parallel(n_jobs=NCORES)(
-            delayed(process)(scenario, sample, n) for sample in samples)
-    #  print(values)
+    values = Parallel(n_jobs=1 if VISUAL else NCORES)(
+            delayed(process)(scenario, sample, visual=VISUAL, **kwargs)
+            for sample in samples)
+    print(values)
     root = os.path.splitext(spath)[0]
     np.save(root + "-labels.npy", values)
 
