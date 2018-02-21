facb9ed1251554edc5de729dabbc146c2feec4a9
Robin Roussel <robin.roussel.15@ucl.ac.uk>
2018-02-21 16:33:19 +0000

    Propagate changes to the shell script

--- xp/learning_robustness/run_pipeline.sh
+++ xp/learning_robustness/run_pipeline.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
-METHOD=4
-NSAMPLES=1200
-NPREV=0
-DIR=data/20171220/
+# Usage: ./run_pipeline.sh . 4 1000 ndoms 7
+DIR=$(readlink -e "$1")
+shift
+METHOD="$1"
+shift
+NSAMPLES="$1"
+shift
+KWARGS="$@"
+BASE=S"$METHOD"H-"$NSAMPLES"
 
 python gen_training_set.py $METHOD $NSAMPLES
-mv S"$METHOD"H-"$NSAMPLES"samples.npy $DIR
-python process_training_set.py "$DIR"S"$METHOD"H-"$NSAMPLES"samples.npy $METHOD $NPREV
-python train_estimator.py "$DIR"S"$METHOD"H-"$NSAMPLES"samples.npy "$DIR"S"$METHOD"H-"$NSAMPLES"samples-labels.npy
-python eval_training.py "$DIR"S"$METHOD"H-"$NSAMPLES"samples-classifier.pkl "$DIR"candidates.pkl
+mv -t $DIR $BASE-training.npy $BASE-test.npy
+TRAIN=$DIR/$BASE-training.npy
+TEST=$DIR/$BASE-test.npy
+
+python process_training_set.py $TRAIN $METHOD $KWARGS
+python process_training_set.py $TEST $METHOD $KWARGS
+TRAIN_LABELS=$DIR/$BASE-training-labels.npy
+TEST_LABELS=$DIR/$BASE-test-labels.npy
+
+python train_estimator.py $TRAIN $TRAIN_LABELS
+ESTIMATOR=$DIR/$BASE-classifier.pkl
+
+python eval_training.py $ESTIMATOR $TEST $TEST_LABELS
