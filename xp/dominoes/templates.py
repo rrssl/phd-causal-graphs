7196cc79e404665000dc1f97d43ce209c1177b7e
Robin Roussel <robin.roussel.15@ucl.ac.uk>
2018-03-22 10:58:34 +0000

    Forgot something 3 commits ago

--- xp/dominoes/templates.py
+++ xp/dominoes/templates.py
@@ -90,3 +90,30 @@ def create_circular_arc(center, radius, angle_start, angle_stop, n_doms):
     coords[:, 1] = radius * np.sin(angles_rad)
     _linear_transform_2D(coords, center, 0)
     return coords
+
+
+def create_x_switch(origin, angle, width, n_doms):
+    n_turn = 3
+    coords = np.zeros((2*n_doms, 3))
+    coords[:n_turn] = create_circular_arc(
+        [0, -width/2], width/2, 90, 45, n_turn
+    )
+    coords[n_turn:n_doms-n_turn] = create_line(
+        coords[n_turn-1, :2], -45, width, n_doms-2*n_turn+2
+        )[1:-1]
+    coords[n_doms-n_turn:n_doms] = create_circular_arc(
+        [np.sqrt(2)*width, -width/2], width/2, -135, -90, n_turn
+    )
+    coords[n_doms:n_doms+n_turn] = create_circular_arc(
+        [0, -width/2], width/2, -90, -45, n_turn
+    )
+    coords[n_doms+n_turn:-n_turn] = create_line(
+        coords[n_doms+n_turn-1, :2], 45, width, n_doms-2*n_turn+2
+        )[1:-1]
+    coords[-n_turn:] = create_circular_arc(
+        [np.sqrt(2)*width, -width/2], width/2, 135, 90, n_turn
+    )
+    coords[(n_doms-1)//2, 2] = 0
+    coords = np.delete(coords, n_doms+(n_doms-1)/2, 0)
+    _linear_transform_2D(coords, origin, 0)
+    return coords
