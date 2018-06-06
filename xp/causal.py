ba05f691058bccbe5b92f6f322e83c744762c601
Robin Roussel <robin.roussel.15@ucl.ac.uk>
2018-06-06 22:49:26 +0100

    Add compact virew for the CausalGraphViewer

--- xp/causal.py
+++ xp/causal.py
@@ -137,14 +137,14 @@ class CausalGraphTraverser:
 
     def get_events(self):
         to_traverse = {self.root}
-        traversed = set()
+        traversed = []
         while to_traverse:
             event = to_traverse.pop()
             if event.outcome:
                 for trans in event.outcome.transitions:
                     if trans.dest not in traversed:
                         to_traverse.add(trans.dest)
-            traversed.add(event)
+            traversed.append(event)
         return traversed
 
     def reset(self):
@@ -203,24 +203,49 @@ class CausalGraphViewer:
             EventState.failure: 'orange',
         }
 
-    def render(self, filename=None):
+    def render(self, filename=None, compact=True):
         filename = filename if filename else self._file.name
         g = Digraph('G', filename=filename)
+        g.attr('node', shape='circle')
+        g.attr('node', fontname='Linux Biolinum O')
         to_process = {self.root}
         processed = set()
+        if compact:
+            self._labels = {}
+            label = 'A'
+            for event in CausalGraphTraverser(self.root).get_events():
+                self._labels[event.name] = label
+                label = increment_str(label)
         while to_process:
             event = to_process.pop()
             if event in processed:
                 continue
-            g.node(event.name, style='filled', color='black',
+            node_label = self._labels[event.name] if compact else event.name
+            g.node(node_label, style='filled', color='black',
                    fillcolor=self.colors[event.state])
             if event.outcome:
                 for trans in event.outcome.transitions:
-                    g.edge(event.name, trans.dest.name,
-                           label=str(int(trans.active)))
+                    edge_label = str(int(trans.active)) if not compact else None
+                    dest_label = self._labels[trans.dest.name] if compact else trans.dest.name
+                    g.edge(node_label, dest_label, label=edge_label)
                     to_process.add(trans.dest)
             processed.add(event)
         g.view()
+
+
+def increment_char(c):
+    """
+    Increment an uppercase character, returning 'A' if 'Z' is given
+    """
+    return chr(ord(c) + 1) if c != 'Z' else 'A'
+
+
+def increment_str(s):
+    lpart = s.rstrip('Z')
+    num_replacements = len(s) - len(lpart)
+    new_s = lpart[:-1] + increment_char(lpart[-1]) if lpart else 'A'
+    new_s += 'A' * num_replacements
+    return new_s
 
 
 def connect(source, dest):
