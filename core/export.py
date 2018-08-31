e39cbeccb8f17562e40e6c48833a13a1aa576ab4
Robin Roussel <robin.roussel.15@ucl.ac.uk>
2018-08-31 19:17:19 +0100

    Add linecolor option to all vector shapes

--- core/export.py
+++ core/export.py
@@ -21,17 +21,17 @@ class VectorFile:
                 filename, size=(width + 'cm', height + 'cm'),
                 viewBox='0 0 ' + width + ' ' + height)
 
-    def add_circles(self, positions, radii):
+    def add_circles(self, positions, radii, linecolor='black'):
         cont = self.cont
-        group = cont.add(cont.g(fill='none', stroke='black',
+        group = cont.add(cont.g(fill='none', stroke=linecolor,
                          stroke_width=self.stroke_width))
         for pos, radius in zip(positions, radii):
             circle = cont.circle(center=(pos[0], pos[1]), r=radius)
             group.add(circle)
 
-    def add_rectangles(self, positions, angles, sizes):
+    def add_rectangles(self, positions, angles, sizes, linecolor='black'):
         cont = self.cont
-        group = cont.add(cont.g(fill='none', stroke='black',
+        group = cont.add(cont.g(fill='none', stroke=linecolor,
                          stroke_width=self.stroke_width))
         for pos, angle, size in zip(positions, angles, sizes):
             rect = cont.rect(insert=(pos[0]-size[0]/2, pos[1]-size[1]/2),
@@ -39,10 +39,10 @@ class VectorFile:
             rect.rotate(angle, pos)
             group.add(rect)
 
-    def add_polyline(self, points):
+    def add_polyline(self, points, linecolor='black'):
         cont = self.cont
         cont.add(cont.polyline(
-            points=points, fill='none', stroke='black',
+            points=points, fill='none', stroke=linecolor,
             stroke_width=self.stroke_width
         ))
 
