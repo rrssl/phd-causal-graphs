8d0c327761605e1126d92839c51330f9adaf57d3
Robin Roussel <robin.roussel.15@ucl.ac.uk>
2018-09-11 17:30:30 +0100

    Make export_simu accept paths as parameters

--- demos/export_simu.py
+++ demos/export_simu.py
@@ -1,18 +1,28 @@
+import json
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
-from core.scenario import load, simulate_scene, StateObserver  # noqa:E402
+from core.scenario import (StateObserver, load_scenario,  # noqa:E402
+                           simulate_scene)
+
 
 FPS = 240
+DURATION = 2
 
 
 def main():
-    scenario = load("../scenarios/simple.json", geom='HD')
-    out_dir = "data/export_simu/"
+    if len(sys.argv) < 3:
+        return
+    json_path = sys.argv[1]
+    out_dir = sys.argv[2]
+    with open(json_path, 'r') as f:
+        scenario_data = json.load(f)
+    scenario = load_scenario(scenario_data, geom='HD', phys=False)
     scenario.scene.export_scene_to_egg(out_dir + "scene.egg")
+    scenario = load_scenario(scenario_data, geom=None, phys=True)
     obs = StateObserver(scenario.scene)
-    simulate_scene(scenario.scene, duration=2., timestep=1/FPS,
+    simulate_scene(scenario.scene, duration=DURATION, timestep=1/FPS,
                    callbacks=[obs])
     obs.export(out_dir + "simu.pkl", fps=FPS)
 
