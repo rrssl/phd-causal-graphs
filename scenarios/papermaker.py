08a1c3b87c3feb8d60be4ef24c799613ea1a3800
Robin Roussel <robin.roussel.15@ucl.ac.uk>
2019-01-07 10:22:30 +0000

    Add first version of PaperMaker

---
+++ scenarios/papermaker.py
@@ -0,0 +1,176 @@
+DATA = {
+    'scene': [
+        {
+            'name': "initial_run_support",
+            'type': "Box",
+            'args': {
+                'extents': [],
+            }
+        },
+        {
+            'name': "initial_run",
+            'type': "DominoRun",
+            'args': {
+                'extents': [],
+                'coords': [],
+                'b_mass': 0
+            }
+        },
+        {
+            'name': "ball1",
+            'type': "Ball",
+            'args': {
+                'radius': 0,
+                'b_mass': 0,
+                'b_restitution': 0
+            }
+        },
+        {
+            'name': "ball1_track",
+            'type': "Track",
+            'args': {
+                'extents': []
+            }
+        },
+        {
+            'name': "ball1_bouncer",
+            'type': "Box",
+            'args': {
+                'extents': [],
+                'b_restitution': 0,
+            }
+        },
+        {
+            'name': "ball1_goblet",
+            'type': "Goblet",
+            'args': {
+                'extents': [],
+            }
+        },
+        {
+            'name': "lever1",
+            'type': "Lever",
+            'args': {
+                # TODO
+            }
+        },
+        {
+            'name': "lever2",
+            'type': "Lever",
+            'args': {
+                # TODO
+            }
+        },
+        {
+            'name': "falling_plank",
+            'type': "Box",
+            'args': {
+                'extents': [],
+                'b_mass': 0,
+            }
+        },
+        {
+            'name': "falling_plank_support",
+            'type': "Box",
+            'args': {
+                'extents': []
+            }
+        },
+        {
+            'name': "left_obstacle",
+            'type': "Box",
+            'args': {
+                'extents': []
+            }
+        },
+        {
+            'name': "right_obstacle",
+            'type': "Box",
+            'args': {
+                'extents': []
+            }
+        },
+        {
+            'name': "lever3",
+            'type': "Lever",
+            'args': {
+                # TODO
+            }
+        },
+        {
+            'name': "lever3_goblet",
+            'type': "Goblet",
+            'args': {
+                'extents': [],
+            }
+        },
+        {
+            'name': "pushing_plank",
+            'type': "Box",
+            'args': {
+                'extents': [],
+                'b_mass': 0
+            }
+        },
+        {
+            'name': "pushing_plank_left_guide",
+            'type': "Box",
+            'args': {
+                'extents': []
+            }
+        },
+        {
+            'name': "pushing_plank_right_guide",
+            'type': "Box",
+            'args': {
+                'extents': []
+            }
+        },
+        {
+            'name': "lever4",
+            'type': "Lever",
+            'args': {
+                # TODO
+            }
+        },
+        {
+            'name': "ball2_track",
+            'type': "Track",
+            'args': {
+                'extents': []
+            }
+        },
+        {
+            'name': "ball2",
+            'type': "Ball",
+            'args': {
+                'radius': 0,
+                'b_mass': 0,
+                'b_restitution': 0
+            }
+        },
+        {
+            'name': "ball2_bouncer1",
+            'type': "Box",
+            'args': {
+                'extents': [],
+                'b_restitution': 0
+            }
+        },
+        {
+            'name': "ball2_bouncer2",
+            'type': "Box",
+            'args': {
+                'extents': [],
+                'b_restitution': 0
+            }
+        },
+        {
+            'name': "ball2_goblet",
+            'type': "Goblet",
+            'args': {
+                'extents': [],
+            }
+        },
+    ]
+}
