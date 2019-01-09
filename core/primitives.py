5dfb5ee74ddd5b3e04060e6446be1c37eaa9a7d3
Robin Roussel <robin.roussel.15@ucl.ac.uk>
2019-01-09 19:58:45 +0000

    Add a Fastener to the primitives

--- core/primitives.py
+++ core/primitives.py
@@ -447,6 +447,42 @@ class Capsule(PrimitiveBase):
         return geom_node
 
 
+class Fastener(PrimitiveBase):
+    """Glue two primitives together.
+
+    Parameters
+    ----------
+    name : string
+      Name of the primitive (useless here).
+    comp1_xform : (6,) float sequence
+      Relative transform of the constraint wrt the first primitive.
+    comp2_xform : (6,) float sequence
+      Relative transform of the constraint wrt the second primitive.
+
+    """
+
+    def __init__(self, name, comp1_xform, comp2_xform):
+        super().__init__(name)
+        self.comp1_xform = TransformState.make_pos_hpr(
+            Point3(*comp1_xform[:3]), Vec3(*comp1_xform[3:])
+        )
+        self.comp2_xform = TransformState.make_pos_hpr(
+            Point3(*comp2_xform[:3]), Vec3(*comp2_xform[3:])
+        )
+
+    def create(self, geom, phys, parent=None, world=None, components=None):
+        if not phys or not components:
+            return
+        comp1, comp2 = components
+        cs = bt.BulletGenericConstraint(
+            comp1.node(), comp2.node(), self.comp1_xform, self.comp2_xform, 1
+        )
+        for i in range(3):
+            cs.set_angular_limit(i, 0, 0)
+            cs.set_linear_limit(i, 0, 0)
+        self._attach(constraints=[cs], world=world)
+
+
 class Pivot(PrimitiveBase):
     """Attach a pivot constraint to a primitive.
 
