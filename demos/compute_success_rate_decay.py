179942fd42d429c50b0cb790e63011af70ec61bc
Robin Roussel <robin.roussel.15@ucl.ac.uk>
2019-01-13 15:00:35 +0000

    Make various change to robustness curve script
    Too many to list...

--- demos/compute_success_rate_decay.py
+++ demos/compute_success_rate_decay.py
@@ -21,14 +21,15 @@ from bayes_opt import BayesianOptimization, UtilityFunction
 from joblib import delayed, Memory, Parallel
 from scipy.stats import sem
 from timeit import default_timer as timer
+from tqdm import tqdm
 
 sys.path.insert(0, os.path.abspath(".."))
+import core.optimize as opt  # noqa: E402
 import core.robustness as rob  # noqa: E402
-from core.optimize import maximize_robustness2  # noqa: E402
-from core.scenario import (import_scenario_data, load_scenario)  # noqa: E402
+from core.scenario import import_scenario_data, load_scenario  # noqa: E402
+from core.config import NCORES  # noqa: E402
 
 SIMU_KW = dict(timestep=1/500, duration=0)
-NCORES = 2
 memory = Memory(cachedir=".cache")
 
 
@@ -54,11 +55,30 @@ def generate_dataset(scenario, n_samples):
     return X, y, t
 
 
+@memory.cache
+def generate_dataset2(scenario, n_samples):
+    t = timer()
+    n_dims = len(scenario.design_space)
+    X = rob.SobolSequence(n_dims).sample(n_samples)
+    # labels = [compute_label(scenario, x) for x in X]
+    y = Parallel(n_jobs=NCORES)(delayed(compute_label)(scenario, x)
+                                for x in tqdm(X))
+    t = timer() - t
+    X = np.asarray(X)
+    y = np.asarray(y)
+    return X, y, t
+
+
 def compute_success_rates(X, y, center, radii):
     valid = np.flatnonzero(y)
     y = y[valid]
     d = np.linalg.norm(X[valid] - center, axis=1)
-    return [(y[d <= r] == 1).mean() for r in radii]
+    sr = np.zeros(len(radii))
+    for i, r in enumerate(radii):
+        y_in_ball = y[d <= r]
+        if y_in_ball.size:
+            sr[i] = (y_in_ball == 1).mean()
+    return sr
 
 
 class LocalRobustnessEstimator:
@@ -137,93 +157,136 @@ def find_best_gpo(rob_est, n_eval, xi=0., acq='ei', seed=None):
 
 
 @memory.cache
-def find_successful_samples_adaptive(scenario, n_succ=100, n_0=50, n_k=10,
-                                     k_max=500, sigma=.01, seed=None):
+def find_successful_samples_adaptive(scenario, n_succ, n_0, n_k, k_max, sigma,
+                                     seed=None):
     # 'seed' is just here to cache several results
+    #
     return rob.find_successful_samples_adaptive(
         scenario, n_succ, n_0, n_k, k_max, sigma, **SIMU_KW
     )
 
 
 @memory.cache
-def find_best_ours(scenario, seed=None, ret_simu_cost=False):
+def train_and_consolidate_boundary(scenario, X, y, accuracy, n_k, k_max,
+                                   ret_simu_cost=False):
+    #
+    step_data = [] if ret_simu_cost else None
+    rob_est = rob.train_and_consolidate_boundary2(
+        scenario, X, y, accuracy, n_k, k_max, step_data=step_data, **SIMU_KW
+    )
+    if ret_simu_cost:
+        simu_cost = len(step_data[-1][0])
+        return rob_est, simu_cost
+    else:
+        return rob_est
+
+
+@memory.cache
+def find_best_ours(scenario, active=True, optimizer='global', seed=None,
+                   ret_simu_cost=False):
     if seed is not None:
         np.random.seed(seed)
     # Initialize
+    #
     X, y = find_successful_samples_adaptive(
-        scenario, n_succ=100, n_0=50, n_k=10, k_max=500, sigma=.01, seed=seed
+        scenario, n_succ=100, n_0=500, n_k=10, k_max=500, sigma=.01, seed=seed
     )
-    # Build the robustness estimator
-    # step_data = [] if ret_simu_cost else None
-    # rob_est = rob.train_and_consolidate_boundary2(
-    #     scenario, X, y, accuracy=.85, n_k=50, k_max=1, step_data=step_data,
-    #     **SIMU_KW
-    # )
-    rob_est = rob.train_svc(X, y, probability=True, verbose=False)
-    # Find optimal solution
-    init_probas = rob_est.predict_proba(X)[:, 1]
-    x0 = X[np.argmax(init_probas)]
-    x = x0
-    # x = maximize_robustness(scenario, [rob_est], x0).x
     if ret_simu_cost:
-        # simu_cost = step_data[-1][0].shape[0]
         simu_cost = len(y)
+    # Build the robustness estimator
+    if active:
+        if ret_simu_cost:
+            rob_est, simu_cost = train_and_consolidate_boundary(
+                scenario, X, y, accuracy=1, n_k=10, k_max=5,
+                ret_simu_cost=True
+            )
+        else:
+            rob_est = train_and_consolidate_boundary(
+                scenario, X, y, accuracy=1, n_k=10, k_max=5,
+                ret_simu_cost=False
+            )
+    else:
+        rob_est = rob.train_svc(X, y, probability=True, verbose=False)
+    # Find optimal solution
+    init_probas = rob_est.predict_proba(X)[:, 1]
+    if optimizer is None:
+        x = X[np.argmax(init_probas)]
+    elif optimizer == 'local':
+        x0 = X[np.argmax(init_probas)]
+        res = opt.maximize_robustness_local(scenario, [rob_est], x0)
+        x = res.x
+    elif optimizer == 'global':
+        # init = np.asarray(X)[np.argpartition(init_probas, -100)[-100:]]
+        x0 = X[np.argmax(init_probas)]
+        res = opt.maximize_robustness_global(scenario, [rob_est], x0,
+                                             fevals=100, **SIMU_KW)
+        x = res[0]
+        simu_cost += res[3]
+    if ret_simu_cost:
         return x, simu_cost
     else:
         return x
 
 
 @memory.cache
-def find_best_ours2(scenario, seed=None, ret_simu_cost=False):
-    if seed is not None:
-        np.random.seed(seed)
-    # Initialize
-    X, y = find_successful_samples_adaptive(
-        scenario, n_succ=100, n_0=50, n_k=10, k_max=500, sigma=.01, seed=seed
-    )
-    # Build the robustness estimator
-    step_data = [] if ret_simu_cost else None
-    rob_est = rob.train_and_consolidate_boundary2(
-        scenario, X, y, accuracy=1, n_k=10, k_max=5, step_data=step_data,
-        **SIMU_KW
-    )
-    # Find optimal solution
-    init_probas = rob_est.predict_proba(X)[:, 1]
-    x0 = X[np.argmax(init_probas)]
-    x = x0
-    # x = maximize_robustness(scenario, [rob_est], x0).x
-    if ret_simu_cost:
-        simu_cost = step_data[-1][0].shape[0]
-        return x, simu_cost
-    else:
-        return x
+def compute_ours(X, y, errors, n_runs, scenario, active=True,
+                 optimizer='local', trial=0):
+    curves = []
+    simu_costs = []
+    for i in range(n_runs):
+        x_ours, simu_cost = find_best_ours(
+            scenario, active=active, optimizer=optimizer, seed=i,
+            ret_simu_cost=True
+        )
+        X_ours = np.vstack((X, x_ours))
+        y_ours = np.append(y, compute_label(scenario, x_ours))
+        curve = compute_success_rates(X_ours, y_ours, x_ours, errors)
+        curves.append(curve)
+        simu_costs.append(simu_cost)
+    avg_curve = np.mean(curves, axis=0)
+    sem_curve = sem(curves, axis=0)
+    simu_cost = sum(simu_costs) // len(simu_costs)
+    return avg_curve, sem_curve, simu_cost
 
 
 @memory.cache
-def find_best_ours3(scenario, seed=None, ret_simu_cost=False):
-    if seed is not None:
-        np.random.seed(seed)
-    # Initialize
-    X, y = find_successful_samples_adaptive(
-        scenario, n_succ=100, n_0=50, n_k=10, k_max=500, sigma=.01, seed=seed
-    )
-    # Build the robustness estimator
-    step_data = [] if ret_simu_cost else None
-    rob_est = rob.train_and_consolidate_boundary2(
-        scenario, X, y, accuracy=1, n_k=10, k_max=5, step_data=step_data,
-        **SIMU_KW
-    )
-    # Find optimal solution
-    X = np.asarray(X)
-    init_probas = rob_est.predict_proba(X)[:, 1]
-    init = X[np.argpartition(init_probas, -100)[-100:]]
-    res = maximize_robustness2(scenario, [rob_est], init, **SIMU_KW)
-    x = res.x
-    if ret_simu_cost:
-        simu_cost = step_data[-1][0].shape[0]
-        return x, simu_cost
-    else:
-        return x
+def compute_B1(X, y, errors, n_runs):
+    successes = np.flatnonzero(y == 1)[:n_runs]
+    curves = np.array([compute_success_rates(X, y, x, errors)
+                       for x in X[successes]])
+    avg_curve = np.mean(curves, axis=0)
+    sem_curve = sem(curves, axis=0)
+    return avg_curve, sem_curve
+
+
+@memory.cache
+def compute_B2(X, y, errors, n_runs, n_eval, rob_est, trial=0):
+    curves = []
+    for i in range(n_runs):
+        rob_est.n_eval = 0
+        x_uni = find_best_uniform(rob_est, n_eval, seed=i)
+        X_uni = np.vstack((X, x_uni))
+        y_uni = np.append(y, compute_label(rob_est.scenario, x_uni))
+        curve = compute_success_rates(X_uni, y_uni, x_uni, errors)
+        curves.append(curve)
+    avg_curve = np.mean(curves, axis=0)
+    sem_curve = sem(curves, axis=0)
+    return avg_curve, sem_curve
+
+
+@memory.cache
+def compute_B3(X, y, errors, n_runs, n_eval, rob_est, trial=0):
+    curves = []
+    for i in range(n_runs):
+        rob_est.n_eval = 0
+        x_gpo = find_best_gpo(rob_est, n_eval, seed=i)
+        X_gpo = np.vstack((X, x_gpo))
+        y_gpo = np.append(y, compute_label(rob_est.scenario, x_gpo))
+        curve = compute_success_rates(X_gpo, y_gpo, x_gpo, errors)
+        curves.append(curve)
+    avg_curve = np.mean(curves, axis=0)
+    sem_curve = sem(curves, axis=0)
+    return avg_curve, sem_curve
 
 
 def main():
@@ -235,122 +298,70 @@ def main():
     SIMU_KW['duration'] = int(sys.argv[3])
     scenario_data = import_scenario_data(path)
     scenario = load_scenario(scenario_data)
-    X, y, t = generate_dataset(scenario, n_samples)
+    # X, y, t = generate_dataset(scenario, n_samples)
+    X, y, t = generate_dataset2(scenario, n_samples)
     print("Invalid", "Successes", "Failures", "Time")
     print((y == 0).sum(), (y == 1).sum(), (y == -1).sum(), t)
 
     n_dims = X.shape[1]
     n_err = 30  # number of errors to build the success rate plot
     errors = np.linspace(0, 1, n_err)
-    n_runs = 15  # number of runs for each random method
+    n_runs = 10  # number of runs for each random method
     seaborn.set()
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(6, 2))
+
+    # # Compute the decay of the most robust solution of our passive method.
+    # avg_curve, sem_curve, simu_cost = compute_ours(
+    #     X, y, errors, n_runs, scenario, active=False, optimizer=None
+    # )
+    # ax.plot(errors, avg_curve,
+    #         label="Most robust: ours, passive, noopt (n={})".format(n_runs))
+    # ax.fill_between(errors, avg_curve - sem_curve, avg_curve + sem_curve,
+    #                 alpha=.5)
+
+    # # Compute the decay of the most robust solution of our active method.
+    # avg_curve, sem_curve, simu_cost = compute_ours(
+    #     X, y, errors, n_runs, scenario, active=True, optimizer=None
+    # )
+    # ax.plot(errors, avg_curve,
+    #         label="Most robust: ours, active, noopt (n={})".format(n_runs))
+    # ax.fill_between(errors, avg_curve - sem_curve, avg_curve + sem_curve,
+    #                 alpha=.5)
 
     # Compute the decay of the most robust solution of our method.
-    decays = []
-    simu_costs = []
-    for i in range(n_runs):
-        x_ours, simu_cost = find_best_ours(scenario, seed=i,
-                                           ret_simu_cost=True)
-        X_ours = np.vstack((X, x_ours))
-        y_ours = np.append(y, compute_label(scenario, x_ours))
-        decay = compute_success_rates(X_ours, y_ours, x_ours, errors)
-        decays.append(decay)
-        simu_costs.append(simu_cost)
-    avg_decay = np.mean(decays, axis=0)
-    sem_decay = sem(decays, axis=0)
-    ax.plot(errors, avg_decay,
-            label="Most robust: ours, passive (n={})".format(n_runs))
-    ax.fill_between(errors, avg_decay - sem_decay, avg_decay + sem_decay,
+    avg_curve, sem_curve, simu_cost = compute_ours(
+        X, y, errors, n_runs, scenario, active=True, optimizer='local', trial=0
+    )
+    ax.plot(errors, avg_curve, label="Ours")
+    ax.fill_between(errors, avg_curve - sem_curve, avg_curve + sem_curve,
                     alpha=.5)
 
-    # Compute the decay of the most robust solution of our method 2.
-    decays = []
-    simu_costs = []
-    for i in range(n_runs):
-        x_ours, simu_cost = find_best_ours2(scenario, seed=i,
-                                            ret_simu_cost=True)
-        X_ours = np.vstack((X, x_ours))
-        y_ours = np.append(y, compute_label(scenario, x_ours))
-        decay = compute_success_rates(X_ours, y_ours, x_ours, errors)
-        decays.append(decay)
-        simu_costs.append(simu_cost)
-    avg_decay = np.mean(decays, axis=0)
-    sem_decay = sem(decays, axis=0)
-    ax.plot(errors, avg_decay,
-            label="Most robust: ours, active (n={})".format(n_runs))
-    ax.fill_between(errors, avg_decay - sem_decay, avg_decay + sem_decay,
-                    alpha=.5)
+    # simu_budget = 1000  # number of simus allowed for each method
+    simu_budget = simu_cost
+    print("Number of simulations allowed:", simu_budget)
 
-    # Compute the decay of the most robust solution of our method 3.
-    decays = []
-    simu_costs = []
-    for i in range(n_runs):
-        x_ours, simu_cost = find_best_ours3(scenario, seed=i,
-                                            ret_simu_cost=True)
-        X_ours = np.vstack((X, x_ours))
-        y_ours = np.append(y, compute_label(scenario, x_ours))
-        decay = compute_success_rates(X_ours, y_ours, x_ours, errors)
-        decays.append(decay)
-        simu_costs.append(simu_cost)
-    avg_decay = np.mean(decays, axis=0)
-    sem_decay = sem(decays, axis=0)
-    ax.plot(errors, avg_decay,
-            label="Most robust: ours, octive (n={})".format(n_runs))
-    ax.fill_between(errors, avg_decay - sem_decay, avg_decay + sem_decay,
+    # B1: random uniform successes.
+    avg_curve, sem_curve = compute_B1(X, y, errors, n_runs)
+    ax.plot(errors, avg_curve, label="B1")
+    ax.fill_between(errors, avg_curve - sem_curve, avg_curve + sem_curve,
                     alpha=.5)
 
     # Initialize options for baselines based on local robustness evaluation.
-    # simu_budget = 1000  # number of simus allowed for each method
-    simu_budget = sum(simu_costs) // len(simu_costs)
-    print("Number of simulations allowed:", simu_budget)
     radius = .1  # radius of the ball to compute local robustness
     n_neighbors = n_dims**2  # number of simulations to compute local rob
     n_eval = simu_budget // (n_neighbors + 1)  # number of rob eval allowed
     rob_est = LocalRobustnessEstimator(scenario, radius, n_neighbors)
 
-    # Compute the decay of the most robust solution of uniform search.
-    decays = []
-    for i in range(n_runs):
-        rob_est.n_eval = 0
-        x_uni = find_best_uniform(rob_est, n_eval, seed=i)
-        X_uni = np.vstack((X, x_uni))
-        y_uni = np.append(y, compute_label(scenario, x_uni))
-        decay = compute_success_rates(X_uni, y_uni, x_uni, errors)
-        decays.append(decay)
-    avg_decay = np.mean(decays, axis=0)
-    sem_decay = sem(decays, axis=0)
-    ax.plot(errors, avg_decay,
-            label="Most robust: uniform search (n={})".format(n_runs))
-    ax.fill_between(errors, avg_decay - sem_decay, avg_decay + sem_decay,
+    # B2: robustness-based uniform search.
+    avg_curve, sem_curve = compute_B2(X, y, errors, n_runs, n_eval, rob_est)
+    ax.plot(errors, avg_curve, label="B2")
+    ax.fill_between(errors, avg_curve - sem_curve, avg_curve + sem_curve,
                     alpha=.5)
 
-    # Compute the decay of the most robust solution of GP optimization.
-    decays = []
-    for i in range(n_runs):
-        rob_est.n_eval = 0
-        x_gpo = find_best_gpo(rob_est, n_eval, seed=i)
-        X_gpo = np.vstack((X, x_gpo))
-        y_gpo = np.append(y, compute_label(scenario, x_gpo))
-        decay = compute_success_rates(X_gpo, y_gpo, x_gpo, errors)
-        decays.append(decay)
-    avg_decay = np.mean(decays, axis=0)
-    sem_decay = sem(decays, axis=0)
-    ax.plot(errors, avg_decay,
-            label="Most robust: GP optimization (n={})".format(n_runs))
-    ax.fill_between(errors, avg_decay - sem_decay, avg_decay + sem_decay,
-                    alpha=.5)
-
-    # Compute the average decay of random uniform successes.
-    valid = np.flatnonzero(y)[:simu_budget]
-    successes = valid[y[valid] == 1]
-    decays = np.array([compute_success_rates(X, y, x, errors)
-                       for x in X[successes]])
-    avg_decay = np.mean(decays, axis=0)
-    sem_decay = sem(decays, axis=0)
-    ax.plot(errors, avg_decay,
-            label="Random success (n={})".format(successes.size))
-    ax.fill_between(errors, avg_decay - sem_decay, avg_decay + sem_decay,
+    # B3: Bayesian optimization.
+    avg_curve, sem_curve = compute_B3(X, y, errors, n_runs, n_eval, rob_est)
+    ax.plot(errors, avg_curve, label="B3")
+    ax.fill_between(errors, avg_curve - sem_curve, avg_curve + sem_curve,
                     alpha=.5)
 
     ax.legend()
